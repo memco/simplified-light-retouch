ad8b988f9c7434ab9244f427b306ff81728aa61a
memco <mikemckend@gmail.com>
2013-02-17 22:30:03 -0800

    Added ability to single click channel names to open channel

--- scripts.js
+++ scripts.js
@@ -3,13 +3,26 @@
 Textual.viewFinishedLoading = function()
 {
 	Textual.fadeInLoadingScreen(1.00, 0.95);
-	
+
 	setTimeout(function() {
 		Textual.scrollToBottomOfView()
 	}, 500);
+
+	addChannelSingleCLickListener();
 }
 
 Textual.viewFinishedReload = function()
 {
 	Textual.viewFinishedLoading();
+}
+
+function addChannelSingleCLickListener() {
+	var channelLinks = document.getElementsByClassName("channel");
+	for(i=0,len=channelLinks.length;i<len;i++) {
+		channelLinks[i].addEventListener('click',Textual.channelNameDoubleClicked);
+	}
+}
+
+Textual.newMessagePostedToView = function() {
+	addChannelSingleCLickListener();
 }
